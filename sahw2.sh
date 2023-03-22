ed1023ec1297c189d2956613b320e99f83b79aa7
disneyyy <83497508+disneyyy@users.noreply.github.com>
2023-03-22 23:23:27 +0800

    20230322
    add help

--- sahw2.sh
+++ sahw2.sh
@@ -1,1 +1,8 @@
 #!/bin/tcsh
+
+if [ "$#" -eq 1 ] && [ "$1" = "-h" ] ; then
+    echo -n -e "\nUsage: sahw2.sh {--sha256 hashes ... | --md5 hashes ...} -i files ...\n\n--sha256: SHA256 hashes to validate input files.\n--md5:MD5 hashes to validate input files.\n-i: Input files.\n"
+
+else
+    echo "Hello World.\n"
+fi
