6318f983af7f546d611f30654e27d2f3a3b57f08
disneyyy <83497508+disneyyy@users.noreply.github.com>
2023-03-24 01:22:17 +0800

    20230324 01:21
    finish general errors

--- sahw2.sh
+++ sahw2.sh
@@ -1,28 +1,28 @@
 #!/bin/tcsh
 hash_num=-1
 factor=1
+pre="none"
 if [ "$#" -eq 1 ] && [ "$1" = "-h" ] ; then
-    echo -n -e "\nUsage: sahw2.sh {--sha256 hashes ... | --md5 hashes ...} -i files ...\n\n--sha256: SHA256 hashes to validate input files.\n--md5:MD5 hashes to validate input files.\n-i: Input files.\n"
+    echo -n -e "\nUsage: sahw2.sh {--sha256 hashes ... | --md5 hashes ...} -i files ...\n\n--sha256: SHA256 hashes to validate input files.\n--md5: MD5 hashes to validate input files.\n-i: Input files.\n"
     exit 0
-elif [ "$1" = "--md5" ] || [ "$1" = "--sha256" ]; then
+elif [ "$1" = "--md5" ] || [ "$1" = "--sha256" ] ; then
     max=0
     for i in "$@" ; do
         if [ "$i" = "--sha256" ] && [ "$1" = "--md5" ] ; then
-            echo "Error: Only one type of hash function is allowed."
+            echo -n -e "Error: Only one type of hash function is allowed." 1>&2
             exit 1
         elif [ "$i" = "--md5" ] && [ "$1" = "--sha256" ] ; then
-            echo "Error: Only one type of hash function is allowed."
+            echo -n -e "Error: Only one type of hash function is allowed." 1>&2
             exit 1
         elif [ "$i" != "-i" ] ; then
             hash_num=$(( $hash_num + $factor ))
-
         else
             max=${hash_num}
             factor=-1
         fi
     done
     if [ "$hash_num" -ne 0 ] ; then
-        echo "Error: Invalid values."
+        echo -n -e "Error: Invalid values." 1>&2
         exit 1
     else
         echo "correct" 
@@ -30,9 +30,39 @@ elif [ "$1" = "--md5" ] || [ "$1" = "--sha256" ]; then
     # while("$i" != "-i")
     #  
     # md5sum $file | cut -c 1-32
+elif [ "$1" = "-i" ] ; then
+    max=0
+    for i in "$@" ; do
+        if [ "$i" = "--sha256" ] && [ "$pre" = "none" ]; then
+            pre="--sha256"
+            max=${hash_num}
+            factor=-1
+        elif [ "$i" = "--md5" ] && [ "$pre" = "none" ] ; then
+            pre="--md5"
+            max=${hash_num}
+            factor=-1
+        elif [ "$i" != "--sha256" ] && [ "$i" != "--md5" ] ; then
+            hash_num=$(( $hash_num + $factor ))
+        elif [ "$i" = "--md5" ] && [ "$pre" != "--md5" ] ; then
+            echo -n -e "Error: Only one type of hash function is allowed." 1>&2
+            exit 1
+        elif [ "$i" = "--sha256" ] && [ "$pre" != "--sha256" ] ; then
+            echo -n -e "Error: Only one type of hash function is allowed." 1>&2
+            exit 1
+        else
+            echo -n -e "Error: Only one type of hash function is allowed." 1>&2
+            exit 1
+        fi
+    done
+    if [ "$hash_num" -ne 0 ] ; then
+        echo -n -e "Error: Invalid values." 1>&2
+        exit 1
+    else
+        echo "correct" 
+    fi
 else
-    echo "Error: Invalid arguments."
-    echo -n -e "\nUsage: sahw2.sh {--sha256 hashes ... | --md5 hashes ...} -i files ...\n\n--sha256: SHA256 hashes to validate input files.\n--md5:MD5 hashes to validate input files.\n-i: Input files.\n"
+    echo -n -e "\nUsage: sahw2.sh {--sha256 hashes ... | --md5 hashes ...} -i files ...\n\n--sha256: SHA256 hashes to validate input files.\n--md5: MD5 hashes to validate input files.\n-i: Input files.\n"
+    echo -n -e "Error: Invalid arguments." 1>&2
     exit 1
 fi
 # sha256sum $file | cut -c 1-64
